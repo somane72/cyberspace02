3d5726b95c3de46ea3a6fce5bf353ad2b80209d9
isim <hcalik77@hotmail.com>
2020-11-29 16:00:53 +0300

    deneme huseyin

---
+++ src/ikincicalisma/HuseyinGit.java
@@ -0,0 +1,9 @@
+package ikincicalisma;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HuseyinGit {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+    }
+}
